1b4b226d30c70e25733ded459ff6a82fb779c04c
sufyan.arif <sufyan.arif@hmcts.net>
2019-07-09 13:13:24 +0100

    latest docs

--- copyAndPublish.sh
+++ copyAndPublish.sh
@@ -1,8 +1,15 @@
 #!/bin/sh
 
+rm main*
+rm polyfill*
+rm styles*
+rm runtime*
+rm index.html
+rm 3rdpartylicenses.txt
+rm favicon.ico
+
 cp ../cpp.ui.pdk2/dist/docs/* .
 git add -A
 git commit -am "latest docs"
 git push origin master
-
 
